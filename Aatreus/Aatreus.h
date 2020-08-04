f848cc632e57bccff637193f6c26ce0737e77b9a
TaintCleanser <59182354+TaintCleanser@users.noreply.github.com>
2020-08-04 15:27:14 -0400

    Add files via upload

--- Aatreus/Aatreus.h
+++ Aatreus/Aatreus.h
@@ -49,8 +49,8 @@
 { \
 	{ k00, k01, k02, k03, k04, XXX, k05, k06, k07, k08, k09 }, \
 	{ k10, k11, k12, k13, k14, XXX, k15, k16, k17, k18, k19 }, \
-	{ k20, k21, k22, k23, k24, XXX, k36, k25, k26, k27, k28 }, \
-  { k29, k30, k31, k32, k33, XXX, k34, k35, k37, k38, k39 }, \
+	{ k20, k21, k22, k23, k24, XXX, k25, k26, k27, k28, k28 }, \
+  { k30, k31, k32, k33, k34, XXX, k35, k36, k37, k38, k39 }, \
   { k40, k41, k42, k43, k44, XXX, k45, k46, k47, k48, k49 }, \
   { XXX, XXX, XXX, XXX, K54, XXX, k55, XXX, XXX, XXX, XXX }, \
 }
